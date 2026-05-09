498b6739b53cf1ba6db2753719275e0a80fe35c5
QSP <QSP@DESKTOP-K5OA249>
2026-05-09 08:44:24 +0530

    first time committing

---
+++ src/test/java/Demo/Demo1Test.java
@@ -0,0 +1,12 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test {
+
+	@Test(groups = "smoke")
+	public void mangoTest()
+	{
+		System.out.println("mango");
+	}
+}
---
+++ src/test/java/Demo/DemoTest.java
@@ -0,0 +1,14 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	
+	@Test
+	public void appleTest()
+	{
+		System.out.println("apple");
+	}
+	
+}
